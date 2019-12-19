fafbc43a05e35cfc7aa9c2f37058f5a5ea205466
José Rui Barros <joserui@joseruibarros.com>
2019-12-19 20:08:47 +0000

    Added new code that I implemented for now. It's not tested but I need it now in the repo.

--- lcd_alarmclock.ino
+++ lcd_alarmclock.ino
@@ -1,15 +1,29 @@
 #include <SPI.h>
 #include <SparkFunDS3234RTC.h>
 #include <LiquidCrystal.h>
+#include <JC_Button.h>              // https://github.com/JChristensen/JC_Button
+
+#define DS13074_CS_PIN 10 // DeadOn RTC Chip-select pin
+const int CHANGE_PIN = 19; //
+
+// Set pin mode for change button
+Button button1(19);
+Button button2(18);
+Button button3(17);
 
 // Initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-#define DS13074_CS_PIN 10 // DeadOn RTC Chip-select pin
+int currDisplay = 0;
+bool settingMode = false;
 
 void setup() {
+
+  button1.begin();
+  button2.begin();
+  button3.begin();
   
-  //Set up the LCD's number of columns and rows:
+  // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2); 
 
   // Initialize library
@@ -27,16 +41,54 @@ void loop() {
    // Fetch current time from the rtc chip
    rtc.update();
 
+   button1.read();
+   button2.read();   
+   button3.read();
+
+   bool increase = button3.wasReleased();
+   bool decrease = button2.wasReleased();
+   
+   if(button1.pressedFor(1000)){
+       if(currDisplay == 0){
+           settingMode = true;
+       }
+   }
+   else if(button1.wasReleased()){
+      currDisplay++;
+   }
+
+    switch(currDisplay){
+      case 0: printHours(); break;
+      case 1: printKEK(); break;
+      default: currDisplay = 0;
+    }
+
+    if(settingMode){
+      switch(currDisplay){
+        case 0:
+            if(increase){
+              int hour = rtc      
+            }
+      }
+    }
+    
+}
+
+void printKEK(){
+  lcd.print("AAAAAAAAAAAAAAAAAAAAA");
+}
+
+void printHours(){
+  
    // Print everything
    hPrint(rtc.hour());
    lcd.print(":");
    hPrint(rtc.minute());
    lcd.print(":");
    hPrint(rtc.second());
-   
 }
 
-void hPrint(uint8_t i) {
+void hPrint(uint8_t i, bool a) {
   if (i < 10){
     lcd.print("0");
   }
