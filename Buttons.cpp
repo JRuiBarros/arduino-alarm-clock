f0cb1795960fee205da36cf88cc4ec25b314d998
José Rui Barros <joserui@joseruibarros.com>
2020-04-18 18:09:08 +0100

    Buttons.cpp formatting

--- Buttons.cpp
+++ Buttons.cpp
@@ -1,27 +1,33 @@
 #include "Buttons.h"
 
-Buttons::Buttons(int p1, int p2):b1(p1), b2(p2){
+Buttons::Buttons(int p1, int p2) : b1(p1), b2(p2)
+{
   b1.begin();
   b2.begin();
 }
 
-void Buttons::readButtons(){
+void Buttons::readButtons()
+{
   b1.read();
   b2.read();
 }
 
-boolean Buttons::wasB1Released(){
-	return b1.wasReleased();
+boolean Buttons::wasB1Released()
+{
+  return b1.wasReleased();
 }
 
-boolean Buttons::wasB2Released(){
-	return b2.wasReleased();
+boolean Buttons::wasB2Released()
+{
+  return b2.wasReleased();
 }
 
-boolean Buttons::wasB1LongPressed(){
-    return b1.pressedFor(LONG_PRESS);
+boolean Buttons::wasB1LongPressed()
+{
+  return b1.pressedFor(LONG_PRESS);
 }
 
-boolean Buttons::wasB2LongPressed(){
-    return b2.pressedFor(LONG_PRESS);
+boolean Buttons::wasB2LongPressed()
+{
+  return b2.pressedFor(LONG_PRESS);
 }
